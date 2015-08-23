88e4b670ceb4ddd0dbc3e05e4e4f80c997344bb5
Symphorien Gibol <symphorien.gibol@gmail.com>
2015-08-23 23:27:54 +0200

    Rewrite docs of Event field.

--- platalbank_core/models/event.py
+++ platalbank_core/models/event.py
@@ -2,7 +2,9 @@ from django.db import models
 
 class Event(models.Models)
     label = models.CharField(max_length=100)
-    through_khube = models.BooleanField()#if true, one can seek the khube's authorization
-    writable = models.BooleanField() #if false, new transactions cannot be added
+    # if true, authorization requests will be sent to the khube, otherwise to the customer.
+    through_khube = models.BooleanField()
+    # whether new transactions can be added
+    writable = models.BooleanField()
 
 
