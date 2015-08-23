fb6459b5ac00b8e67fa89754ea7db5e16a814bd2
Symphorien Gibol <symphorien.gibol@gmail.com>
2015-08-23 23:27:36 +0200

    Fix typos in platalbank_core/models/transaction.py

--- platalbank_core/models/transaction.py
+++ platalbank_core/models/transaction.py
@@ -8,11 +8,11 @@ class Transaction(models.Model):
     PENDING = "P"
     # when the khube/the customer has acknowledged the transaction ; it still needs to be completed by the seller.
     AUTHORIZED = "A"
-    # when the khube/the customer has rejected the trasaction ; it stil need to be aborted by the seller.
+    # when the khube/the customer has rejected the trasaction ; it still needs to be aborted by the seller.
     REJECTED = "R"
-    #when the seller has decided not to go on with the transaction ; it only appears in specialized history
+    # when the seller has decided not to go on with the transaction ; it only appears in specialized history.
     ABORTED = "X"
-    #when the seller has decided to cash in.
+    # when the seller has decided to cash in.
     COMPLETED = "C"
     _STATE_CHOICES = (
         (PENDING, "PENDING"),
