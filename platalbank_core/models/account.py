9943fc1f4f4286a6cb62e459ce30ea487aa00e8b
Symphorien Gibol <symphorien.gibol@gmail.com>
2015-08-23 22:02:38 +0200

    Add models except Group and Users.

--- platalbank_core/models/account.py
+++ platalbank_core/models/account.py
@@ -1,1 +1,7 @@
 from django.db import models
+
+class Account(models.Model):
+    balance = models.DecimalField(max_digits=20, decimal_places=2)
+    description = models.CharField(max_length=100)
+    short_name = models.CharField(max_length=4)
+    #todo : owner
--- platalbank_core/models/event.py
+++ platalbank_core/models/event.py
@@ -1,1 +1,8 @@
 from django.db import models
+
+class Event(models.Models)
+    label = models.CharField(max_length=100)
+    through_khube = models.BooleanField()#if true, one can seek the khube's authorization
+    writable = models.BooleanField() #if false, new transactions cannot be added
+
+
