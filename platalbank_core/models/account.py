769412c65d439de869d561d22ef3aabb61c3e24d
Symphorien Gibol <symphorien.gibol@gmail.com>
2015-08-25 18:00:36 +0200

    Raise max_length of some Charfields.

--- platalbank_core/models/account.py
+++ platalbank_core/models/account.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Account(models.Model):
     balance = models.DecimalField(max_digits=20, decimal_places=2)
-    description = models.CharField(max_length=100)
-    short_name = models.CharField(max_length=4)
+    description = models.CharField(max_length=1024)
+    short_name = models.CharField(max_length=128)
     #todo : owner
--- platalbank_core/models/event.py
+++ platalbank_core/models/event.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Event(models.Models)
-    label = models.CharField(max_length=100)
+    label = models.CharField(max_length=1024)
     # if true, authorization requests will be sent to the khube, otherwise to the customer.
     through_khube = models.BooleanField()
     # whether new transactions can be added
