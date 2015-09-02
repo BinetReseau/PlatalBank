a3778cb524420272ebd78b2be91f8e12ecd621b0
Symphorien Gibol <symphorien.gibol@gmail.com>
2015-09-02 22:52:59 +0200

    Add stub methods to manage LegalPerson

--- platalbank_auth/models.py
+++ platalbank_auth/models.py
@@ -7,3 +7,14 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     phone_number = models.CharField(max_length=32)
     room = models.CharField(max_length=128)
+
+
+    def get_ro_legalpeople(self):
+        """return all legal people this user has read-only permissions on"""
+        # TODO: LDAP Stuff
+        return tuple()
+
+    def get_rw_legalpeople(self):
+        """return all legal people this user has read-only permissions on"""
+        # TODO: LDAP stuff
+        return tuple()
