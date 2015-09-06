3126e7fbb42662008cb59acbaf13544907c9b4ed
Ekleog <LeoG@spard.me>
2015-09-06 05:35:34 +0200

    Add name to LegalPerson

--- platalbank_core/models/legalperson.py
+++ platalbank_core/models/legalperson.py
@@ -2,4 +2,5 @@ from django.db import models
 from django.conf import settings
 
 class LegalPerson(models.Model):
-    ldap_dn = models.CharField(max_length=1024)
+    dn = models.CharField(max_length=1024)
+    name = models.CharField(max_length=1024) # FIXME FIXME FIXME: Pull from LDAP -- using django-ldapdb?
