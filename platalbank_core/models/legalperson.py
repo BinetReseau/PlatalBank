287d5a7c5bb0c264539f9fdafd0a032d80c1aecd
Symphorien Gibol <symphorien.gibol@gmail.com>
2015-09-06 16:40:47 +0200

    perms check as Legalperson methods.

--- platalbank_core/models/legalperson.py
+++ platalbank_core/models/legalperson.py
@@ -4,3 +4,18 @@ from django.conf import settings
 class LegalPerson(models.Model):
     dn = models.CharField(max_length=1024)
     name = models.CharField(max_length=1024) # FIXME FIXME FIXME: Pull from LDAP -- using django-ldapdb?
+
+    def may_access(self, user):
+        """whether a user may read the details of this legalperson's accounts.
+roughly read-only permissions
+"""
+        # TODO: LDAP stuff
+        return True
+
+    def may_modify(self, user):
+        """whether a user can add new trasactions to events owned by this legalperson.
+roughly read-write permissions
+"""
+        # TODO: LDAP stuff
+        return True
+
