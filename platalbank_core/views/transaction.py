f67d334302db259633d9af7912c7da01090b3044
Akamine <kalaspa@gmail.com>
2015-11-08 21:12:31 +0100

    mise en place de fonctions de changements d'etat

--- platalbank_core/views/transaction.py
+++ platalbank_core/views/transaction.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import viewsets , exceptions , decorators
 
 from platalbank_core.models import Transaction
 from platalbank_core.serializers import TransactionSerializer
@@ -6,3 +7,29 @@ from platalbank_core.serializers import TransactionSerializer
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+
+    @decorators.detail_route(methods="post")
+    def setState(self, request, pk=None):
+
+        try :
+            transaction = Transaction.object.get(id=pk)
+        except Transaction.doesNotExist:
+            return HttpResponse(status=404)
+
+        #Une fois les permissions implementees, ce type de changement d'etat demandera des permissions Khube ou User
+        if (request.state in [Transaction.AUTHORIZED , Transaction.REJECTED]):
+            transaction.state = Transaction._STATE_CHOICES(request.state)
+            transaction.save()
+
+            return Response({'status':'Transaction {0}'.format(Tansaction._STATE_CHOICES(request.state))})
+
+        #Ce type de changement d'etat demandera des permissions seller
+        elif (request.state in [Transaction.ABORTED,Transaction.COMPLETED]):
+            transaction.state = Transaction._STATE_CHOICES(request.state)
+
+            transaction.save()
+
+            return Response({'status':'Transaction {0}'.format(Tansaction._STATE_CHOICES(request.state))})
+
+        else :
+            return exceptions.PermissionDenied()
