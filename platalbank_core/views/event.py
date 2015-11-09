ffaab3abb6aab1f3d44dd8f7c0c4f8083f14bbf6
Akamine <kalaspa@gmail.com>
2015-11-09 14:09:04 +0100

    Allows event to be closed

--- platalbank_core/views/event.py
+++ platalbank_core/views/event.py
@@ -1,4 +1,7 @@
+from rest_framework.response import Response
 from rest_framework import viewsets
+from rest_framework.decorators import detail_route
+from django.http import HttpResponse
 
 from platalbank_core.models import Event
 from platalbank_core.serializers import EventSerializer
@@ -6,3 +9,18 @@ from platalbank_core.serializers import EventSerializer
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+
+    @detail_route(methods=["post"])
+    def close(self, request , pk=None):
+    	try:
+    		event = Event.objects.get(id=pk)
+    	except Event.DoesNotExist:
+    		HttpResponse(status=404)
+
+    	#On prevoit l'emplacement logique pour une verification de la permission
+    	if True:
+    		event.writable = False
+    		event.save()
+    		return Response({'status':'Event closed'})
+    	else :
+    		raise exceptions.PermissionDenied
